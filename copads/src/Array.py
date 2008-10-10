8444692192f0300da923345454a24bc6d5c60f8d
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2008-10-10 05:28:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/copads/code@75 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/src/Array.py
+++ copads/src/Array.py
@@ -1,21 +1,33 @@
 from CopadsExceptions import ArrayError
 
 class ParallelArray(object):
-    def __init__(self, **kwargs):
+    """
+    Parallel Array is an array whereby each data list in the array is of the
+    same size.
+    Ref: http://en.wikipedia.org/wiki/Parallel_array
+    """
+    def __init__(self, fields = None):
+        """
+        Constructor. Able to initiate the array with a list of fields names
+        """
         self.data = {}
         self.fields = []
-        if kwargs.has_key('fields'): self.initFields(kwargs['fields'])
+        if fields: self.initFields(kwargs['fields'])
     
     def initFields(self, fields):
+        """
+        Initiates a list of fields into the array."""
         if type(fields) <> type([]):
             raise ArrayError('field parameter must be a list, ' + \
                 str(type(fields)) + ' given.')
-        for field in fields: self.data[field] = []
-        self.fields = fields
-        self.field_len = len(fields)
+        for field in fields: self.addField(field)
         
     def addField(self, field):
-        if self.data.has_key('field'):
+        """
+        Method to add a new field into the array. Raises ArrayError if attempt
+        to add an existing field.
+        """
+        if self.data.has_key(field):
             raise ArrayError(str(field) + ' existed.')
         if type(field) <> type('string'):
             raise ArrayError('field must be a string')
@@ -26,4 +38,23 @@ class ParallelArray(object):
             self.fields.append(field)
             self.field_len = len(self.fields)
             
-    
+    def removeField(self, field):
+        """
+        Removes a field, together with its data, from the array."""
+        try:
+            self.data.pop(field)
+            self.fields.remove(field)
+            self.field_len = len(self.fields)
+        except KeyError: pass
+        
+    def changeField(self, oldname, newname):
+        """
+        Change a field name from I(oldname) to I(newname). If I(oldname) does 
+        not exist, a new field of I(newname) will be created."""
+        if self.data.has_key(newname):
+            raise ArrayError(str(newname) + ' already exist in array.')
+        if self.data.has_key(oldname):
+            temp = self.data[oldname]
+            self.removeField(oldname)
+            self.data[newname] = temp
+        else: self.addField(newname)
