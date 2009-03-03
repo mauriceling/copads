2f0724d1e89ba1d4df9e4a251094cb7b630adf67
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2009-03-03 16:04:52 +0000

    epydoc processing
    git-svn-id: svn://svn.code.sf.net/p/copads/code@117 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

---
+++ copads/epydoc.bat
@@ -0,0 +1,1 @@
+python c:\Python25\Scripts\epydoc.py --html --output=d:\copads\doc\html --name="COPADS - Collection of Python Algorithms and Data Structures" --url=http://copads.sourceforge.net --show-imports copads test
