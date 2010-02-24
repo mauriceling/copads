123600e526cd76bbd58a697f6fdf3615a26dfd2f
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2010-02-24 09:56:58 +0000

    show errors in markup
    git-svn-id: svn://svn.code.sf.net/p/copads/code@197 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/epydoc.bat
+++ copads/epydoc.bat
@@ -1,1 +1,1 @@
-python "d:\Program Files\Python26\Scripts\epydoc.py" --html --output=d:\copads\doc\html --name="COPADS - Collection of Python Algorithms and Data Structures" --url=http://copads.sourceforge.net --show-imports copads
+python "d:\Program Files\Python26\Scripts\epydoc.py" -v --html --output=d:\copads\doc\html --name="COPADS - Collection of Python Algorithms and Data Structures" --url=http://copads.sourceforge.net --show-imports copads
