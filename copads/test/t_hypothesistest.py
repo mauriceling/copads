4b88cec6753dee0275faf943670bfea429df144d
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2010-02-24 03:39:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/copads/code@190 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/test/t_hypothesistest.py
+++ copads/test/t_hypothesistest.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 sys.path.append(os.path.join(os.path.dirname(os.getcwd()), 'copads'))
-import hypothesistest as N
+import hypothesis as N
     
 class testNormalDistribution(unittest.TestCase):
     
