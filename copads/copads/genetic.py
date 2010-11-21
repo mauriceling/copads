fd168d8ebb0413be01e369aaae05916d8ce61ded
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2010-11-21 12:38:41 +0000

    added backgrount_mutation to population_data
    git-svn-id: svn://svn.code.sf.net/p/copads/code@254 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/copads/genetic.py
+++ copads/copads/genetic.py
@@ -529,6 +529,7 @@ population_data = \
     'chromosome_length' : 200,
     'chromosome_type' : 'defined',
     'chromosome' : [1] * 200,
+    'background_mutation' : 0.0001,
     'genome_size' : 1,
     'population_size' : 200,
     'fitness_function' : 'default',
@@ -543,7 +544,9 @@ population_data = \
 }
 
 def population_constructor(data=population_data):
-    chr = Chromosome(data['chromosome'], data['nucleotide_list'])
+    chr = Chromosome(data['chromosome'], 
+                     data['nucleotide_list'],
+                     data['background_mutation'])
     org = Organism([chr]*data['genome_size'])
     if data['fitness_function'] != 'default':
         Organism.fitness = data['fitness_function']
