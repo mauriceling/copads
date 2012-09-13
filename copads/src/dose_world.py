c284c6c87d44c6928e60dd24244dbbad32e25ecc
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2012-09-13 23:58:33 +0000

    added comments
    git-svn-id: svn://svn.code.sf.net/p/copads/code@370 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/src/dose_world.py
+++ copads/src/dose_world.py
@@ -7,11 +7,41 @@ import copy
 
 class World(object):
     '''
-    Representation of a 3-dimensional world.
+    Representation of a 3-dimensional ecological world.
+    
+    The ecosystem is made up of ecological cells. Each ecological cell is
+    modelled as a dictionary of 
+    - local_input: A list containing processed input, representing the partial
+        local ecological condition, to be used as input to the organisms in the
+        current ecological cell. This is updated by World.update_local function.
+    - local_output: A list containing processed output, representing the partial
+        local ecological condition. This is updated by World.update_local 
+        function.
+    - temporary_input: A list acting as temporary holding for input after being
+        fed to the organisms in the current ecological cell, which is to be used
+        to update local_input and local_output lists by World.update_local and
+        World.update_ecology functions.
+    - temporary_output: A list acting as temporary holding for output from the 
+        organisms in the current ecological cell, which is to be used to update 
+        local_input and local_output lists by World.update_local and
+        World.update_ecology functions.
+    - organisms: The number of organisms in the current ecological cell which 
+        is updated by World.organism_movement and World.organism_location
+        functions.
     '''
     ecosystem = {}
     
     def __init__(self, world_x, world_y, world_z):
+        '''
+        Setting up the world and ecosystem
+        
+        @param world_x: number of ecological cells on the x-axis
+        @type world_x: integer
+        @param world_y: number of ecological cells on the y-axis
+        @type world_y: integer
+        @param world_z: number of ecological cells on the z-axis
+        @type world_z: integer
+        '''
         eco_cell = {'local_input': [], 'local_output': [],
                     'temporary_input': [], 'temporary_output': [],
                     'organisms': 0}
@@ -26,16 +56,76 @@ class World(object):
                 eco_x[y] = copy.deepcopy(eco_y)
             self.ecosystem[x] = copy.deepcopy(eco_x)
     
-    def ecoregulate(self): 
+    def ecoregulate(self):
+        '''
+        Function to simulate events to the entire ecosystem. B{This function 
+        may be over-ridden by the inherited class or substituted to cater for 
+        ecological schemes but not an absolute requirement to do so.}
+        '''
         pass
         
     def organism_movement(self, x, y, z): 
+        '''
+        Function to trigger organism movement from current ecological cell
+        to an adjacent ecological cell. B{This function may be over-ridden by 
+        the inherited class or substituted to cater for mobility schemes but 
+        not an absolute requirement to do so.}
+        
+        @param x: location of current ecological cell on the x-axis
+        @type x: integer
+        @param y: location of current ecological cell on the y-axis
+        @type y: integer
+        @param z: location of current ecological cell on the z-axis
+        @type z: integer
+        '''
         pass
     def organism_location(self, x, y, z): 
+        '''
+        Function to trigger organism movement from current ecological cell
+        to a distant ecological cell. B{This function may be over-ridden by 
+        the inherited class or substituted to cater for mobility schemes but 
+        not an absolute requirement to do so.}
+        
+        @param x: location of current ecological cell on the x-axis
+        @type x: integer
+        @param y: location of current ecological cell on the y-axis
+        @type y: integer
+        @param z: location of current ecological cell on the z-axis
+        @type z: integer
+        '''
         pass
     
     def update_ecology(self, x, y, z): 
+        '''
+        Function to process temporary_input and temporary_output from the 
+        activities of the organisms in the current ecological cell into a 
+        local ecological cell condition, and update the ecosystem.
+        B{This function may be over-ridden by the inherited class or substituted 
+        to cater for ecological schemes but not an absolute requirement to do 
+        so.}
+        
+        @param x: location of current ecological cell on the x-axis
+        @type x: integer
+        @param y: location of current ecological cell on the y-axis
+        @type y: integer
+        @param z: location of current ecological cell on the z-axis
+        @type z: integer
+        '''
         pass
         
     def update_local(self, x, y, z): 
+        '''
+        Function to update local ecological cell condition from the 
+        ecosystem.
+        B{This function may be over-ridden by the inherited class or substituted 
+        to cater for ecological schemes but not an absolute requirement to do 
+        so.}
+        
+        @param x: location of current ecological cell on the x-axis
+        @type x: integer
+        @param y: location of current ecological cell on the y-axis
+        @type y: integer
+        @param z: location of current ecological cell on the z-axis
+        @type z: integer
+        '''
         pass
