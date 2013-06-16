a4d897c3b5aa6aeb947fe3ddbd9a1ecc0c0e121a
mauriceling <mauriceling@ee6ebb42-a9c0-48f5-abdd-862e3c4f8715>
2013-06-16 01:09:48 +0000

    updated to version 0.4
    git-svn-id: svn://svn.code.sf.net/p/copads/code@421 ee6ebb42-a9c0-48f5-abdd-862e3c4f8715

--- copads/setup.py
+++ copads/setup.py
@@ -4,13 +4,13 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='copads',
-      version='0.3.1',
+      version='0.4',
       description='Collection of Python Algorithms and Data Structures',
       long_description='Collection of Python Algorithms and Data Structures',
       author='Maurice HT Ling',
       author_email='mauriceling@acm.org',
       url='http://copads.sourceforge.net',
-      #download_url='https://sourceforge.net/projects/copads/files/copads-0.3.zip/download',
+      download_url='http://downloads.sourceforge.net/project/copads/copads-0.4.zip',
       license = 'Python Software Foundation License version 2',
       platform = 'OS independent',
       package_dir = {'copads' : 'src',
