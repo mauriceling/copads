a2ef93839fa03f3e2f9f44445661f3e000af568c
Maurice Ling <mauriceling@acm.org>
2014-05-27 20:07:30 +0800

    corrected import error

--- copads/src/ragaraja.py
+++ copads/src/ragaraja.py
@@ -39,7 +39,7 @@ interpreter terminates itself.
 '''
 import random
 import math
-from copads.samplestatistics import SingleSample
+from samplestatistics import SingleSample
 from lc_bf import increment, decrement
 from lc_bf import forward, backward
 from lc_bf import call_out, accept_predefined
