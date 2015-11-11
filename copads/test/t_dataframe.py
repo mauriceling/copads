5fa65c549d41f9177df5553a739c483b0678b01c
unknown <MauriceLing@BIOE-B224S4070.staff.main.ntu.edu.sg>
2015-11-11 09:46:18 +0800

    + add test for labeled series

--- copads/test/t_dataframe.py
+++ copads/test/t_dataframe.py
@@ -14,10 +14,17 @@ class testSeries(unittest.TestCase):
         self.assertEqual(df.analyses, {})
     def testAddData1(self):
         df = d.Series('new_series')
-        data = range(10, 20, 1)
+        data = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
         df.addData(data)
-        self.assertTrue(df.data, [10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+        self.assertTrue(df.data, data)
         self.assertTrue(df.label, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    def testAddData2(self):
+        df = d.Series('new_series')
+        data = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+        label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        df.addData(data)
+        self.assertTrue(df.data, data)
+        self.assertTrue(df.label, label)
         
         
 class testDataframe(unittest.TestCase):
