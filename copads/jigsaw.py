93f3be20f3e4412a27bd7f192a9ed73a8d1526ee
Maurice Ling <mauriceling@acm.org>
2016-05-28 22:47:20 +0800

    + adding elaborate descriptions about JES

--- copads/jigsaw.py
+++ copads/jigsaw.py
@@ -1,7 +1,53 @@
 '''
 Jigsaw Encryption System
+
 Date created: 25th May 2016
+
 Licence: Python Software Foundation License version 2
+
+Jigsaw Encryption System (JES) takes its inspiration from jigsaw puzzles. 
+Most cryptography methods is a 1-to-1 file method - during encryption, a 
+single unencrypted file (commonly known as plain text) is encrypted into 
+one encrypted file (commonly known as cipher text) using a key. 
+Decryption does the reverse. Mathematically,
+
+M{encrypt(plaintext, key) --> ciphertext}
+
+M{decrypt(ciphertext, key) --> plaintext}
+
+If encryption and decryption are seen as transformation and de-
+transformation functions, respectively, between plain text and cipher 
+text; the weakest link is in the 1-to-1 file mapping. Brute force attack 
+is possible because all the plain text information exist in the cipher 
+text, though transformed.
+
+JES addresses the 1-to-1 file weakness by using 1-to-N file mapping. This 
+is likened to taking a picture and make a jigsaw puzzle out of it. The 
+complexity of brute force attack increases exponentially with the number 
+of jigsaw pieces. By producing many files, it also makes it possible to 
+transport the encrypted file safely via multiple routes, and this prevents 
+reduces the chances of man-in-the-middle attack. This is like making a 
+1000-piece jigsaw puzzle, shuffle the pieces, load the pieces into 5 
+containers and the assembly instruction in the 6th container. If one 
+container is hijacked, all the information is not compromised. 
+
+Given that the key size of AES-256 is 32 characters long (32 characters 
+of 8 bytes each = 256 bytes) and given that there are 94 characters in 
+the set of mixed alphanumeric with symbols (that is, abcdefghijklmnopqr
+stuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`[]{}|\:;"'
+<>,.?/), the total number of keys is 1.38 x 10**63 (94**32).
+
+How does this number of permutations compare with Jigsaw Encryption?
+
+Assuming that Jigsaw Encryption is used to encrypt a 1 MB (1048576 bytes) 
+file and split into 16 64-KB Jigsaw files, there are 2 x 10**13 
+permutations to assemble 16 64-KB files into the original 1 MB file using 
+Jigsaw version 1. 
+
+Jigsaw version 2 provides 12 variations for each Jigsaw file. This means 
+that there are 2 x 10**14 permutations for the 16 64-KB files. Hence, the 
+full permutations possible for 16 64-KB files is much more than 2 x 
+10**20065 [full permutation for 6000, nPr(6000, 6000), is 2 x 10**20065].
 '''
 
 import os
