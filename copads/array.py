eea02b1945b851db2d40154335d6ef57505e9aa2
unknown <MauriceLing@BIOE-B224S4070.staff.main.ntu.edu.sg>
2016-07-20 15:32:10 +0800

    # edit Array documentation

--- copads/array.py
+++ copads/array.py
@@ -1,45 +1,52 @@
-"""
+'''
 Array Data Structures and Algorithms.
 
 Copyright (c) Maurice H.T. Ling <mauriceling@acm.org>
 
 Date created: 19th March 2008
-"""
+'''
 
 from copadsexceptions import ArrayError
 
 class ParallelArray(object):
-
-    
-    """
+    '''
     Parallel Array is an array whereby each data list in the array is of the
     same size.
     Ref: http://en.wikipedia.org/wiki/Parallel_array
-    """
+    '''
 
-    def __init__(self, fields = None):
-        """Constructor. Able to initiate the array with a list of fields names        
-        @param fields: list of field names to initiate. Default = None"""
+    def __init__(self, fields=None):
+        '''
+        Constructor.       
+        
+        @param fields: field names to initiate. Default = None.
+        @type fields: list
+        '''
         self.data = {}
         self.fields = []
         if fields:
             self.initFields(kwargs['fields'])  
           
     def initFields(self, fields):
-        """Initiates a list of fields into the array.
+        '''
+        Initiates a list of fields into the array.
         
-        @param fields: list of field names to initiate."""
+        @param fields: field names to initiate.
+        @type fields: list
+        '''
         if type(fields) <> type([]):
             raise ArrayError('field parameter must be a list, ' + \
                 str(type(fields)) + ' given.')
         for field in fields: self.addField(field)  
               
     def addField(self, field):
-        """Method to add a new field into the array. Raises ArrayError if 
+        '''
+        Method to add a new field into the array. Raises ArrayError if 
         attempt to add an existing field.
         
         @param field: name of field
-        @type field: string"""
+        @type field: string
+        '''
         if field in self.data:
             raise ArrayError(str(field) + ' existed.')
         if type(field) <> type('string'):
@@ -52,10 +59,12 @@ class ParallelArray(object):
             self.field_len = len(self.fields)
             
     def removeField(self, field):
-        """Removes a field, together with its data, from the array.
+        '''
+        Removes a field, together with its data, from the array.
         
         @param field: name of field
-        @type field: string"""
+        @type field: string
+        '''
         try:
             self.data.pop(field)
             self.fields.remove(field)
@@ -63,13 +72,15 @@ class ParallelArray(object):
         except KeyError: pass
         
     def changeField(self, oldname, newname):
-        """Change a field name.
+        '''
+        Change a field name.
         
         @param oldname: existing name of field
         @type oldname: string
-        @param newname: new name to change to. A new field by this name will
-            be created if 'oldname' is not found
-        @type newname: string"""
+        @param newname: new name to change to. A new field by this name 
+        will be created if 'oldname' is not found
+        @type newname: string
+        '''
         if self.data.has_key(newname):
             raise ArrayError(str(newname) + ' already exist in array.')
         if self.data.has_key(oldname):
@@ -79,10 +90,12 @@ class ParallelArray(object):
         else: self.addField(newname)
         
     def addData(self, fields, data):
-        """Add data into the array
+        '''
+        Add data into the array.
         
         @param fields: ordered list of fields
-        @param data: ordered list of data to add"""
+        @param data: ordered list of data to add
+        '''
         if len(fields) <> len(data):
             raise ArrayError('Field size and data length are different')
         for x in fields:
